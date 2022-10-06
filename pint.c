0c707733e060cbb90a4738b2331844c1a9002566
dani198 <danyleul16@gmail.com>
2022-10-06 07:48:40 +0300

    to you

--- pint.c
+++ pint.c
@@ -7,10 +7,10 @@
 void pint(stack_t **stack, unsigned int line_number)
 {
 if (*stack == NULL)
-	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-		free_list_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-	printf("%d\n", (*stack)->n);
+{
+fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+free_list_stack(*stack);
+exit(EXIT_FAILURE);
+}
+printf("%d\n", (*stack)->n);
 }
